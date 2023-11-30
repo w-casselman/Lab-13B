771c3da5e5421363f817c68b0ff421d2c933b683
Wesley Casselman <w-casselman@onu.edu>
2023-11-30 10:20:43 -0500

    Completed P13B-1

--- Lab 13B/P13B.cpp
+++ Lab 13B/P13B.cpp
@@ -3,3 +3,34 @@
 	Wesley Casselman
 	November 30th of 2023
 */
+
+#include <iostream>
+
+using namespace std;
+
+bool isLeapYear(int year);
+/**
+ isLeapYear – determines whether a given year is a leap year
+ under the Gregorian calendar.
+ @param year the year; expected to be >= 1582
+ @return true if year is a leap year; false otherwise
+*/
+
+int main(void)
+{
+	cout << "Enter year: ";
+	int year;
+	cin >> year;
+
+	cout << endl << isLeapYear(year) << endl;
+
+	return 0;
+}
+
+bool isLeapYear(int year)
+{
+	if (year % 400 == 0) return true;
+	if (year % 100 == 0) return false;
+	if (year % 4 == 0) return true;
+	return false;
+}
