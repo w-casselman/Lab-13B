1998d5f89380eb32a118fe812064317a12ba33f2
Wesley Casselman <w-casselman@onu.edu>
2023-12-02 18:39:01 -0500

    Completed P13B-2

--- Lab 13B/P13B.cpp
+++ Lab 13B/P13B.cpp
@@ -16,13 +16,25 @@ bool isLeapYear(int year);
  @return true if year is a leap year; false otherwise
 */
 
+int daysInMonth(int month, int year);
+/**
+ daysInMonth – determines the number of days in a specified month
+ @param month the month; expected to be in the range [1..12]
+ @param year the year; expected to be >= 1582
+ @return either 28, 29, 30, or 31, based on month and (leap) year
+*/
+
 int main(void)
 {
-	cout << "Enter year: ";
-	int year;
-	cin >> year;
+	while (true)
+	{
+		cout << "Enter year: ";
+		int month, year;
+		cin >> month >> year;
+		if (cin.fail()) break;
 
-	cout << endl << isLeapYear(year) << endl;
+		cout << endl << daysInMonth(month, year) << endl;
+	}
 
 	return 0;
 }
@@ -33,4 +45,18 @@ bool isLeapYear(int year)
 	if (year % 100 == 0) return false;
 	if (year % 4 == 0) return true;
 	return false;
+}
+
+int daysInMonth(int month, int year)
+{
+	switch (month)
+	{
+	case  2: if (isLeapYear(year)) { return 29; }
+		   else { return 28; }
+	case  4: return 30;
+	case  6: return 30;
+	case  9: return 30;
+	case 11: return 30;
+	default: return 31;
+	}
 }
